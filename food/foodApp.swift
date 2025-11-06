ef8219d668c65531e3676210f80336f4596aa355
eliugiraldo <eliugiraldonavia2015@gmail.com>
2025-11-05 20:26:22 -0500

    Initial Commit

---
+++ food/foodApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodApp.swift
+//  food
+//
+//  Created by Gabriel Barzola arana on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct foodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
